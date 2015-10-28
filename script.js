1948118d1a0fb07363acb72c47015c481cee20f2
Gerard van Helden <gerard@zicht.nl>
2015-10-28 20:11:22 +0100

    Added spin (+some cs fixes)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var G = new vector(0, 0.8),
-    CANVAS = new vector(window.innerWidth, window.innerHeight), // gravity
+var G = new Vector(0, 0.8),
+    CANVAS = new Vector(window.innerWidth, window.innerHeight), // gravity
     MAX = 1000,
     B = 0.8, // bounciness
     world = [],
@@ -14,34 +14,40 @@ function each(w, fn) {
     }
 }
 
-function vector(x, y) {
+function Vector(x, y) {
     this.x = x;
     this.y = y;
 }
 
-vector.prototype.add = function(v) {
+Vector.prototype.add = function(v) {
     this.x += v.x;
     this.y += v.y;
-}
+};
 
-vector.prototype.clone = function() {
-    return new vector(this.x, this.y);
-}
+Vector.prototype.clone = function() {
+    return new Vector(this.x, this.y);
+};
 
-function logo(p, r, dir) {
+function Logo(p, r, dir) {
     this.p = p;
     this.r = r;
     this.mass = r * 0.0003;
     this.forces = [dir, G.clone()];
     this.shade = 0;
+    this.angle = 0;
+    this.spin = 0;
 }
 
-logo.prototype.draw = function(ctx) {
+Logo.prototype.draw = function(ctx) {
     ctx.lineWidth = this.r / 5.5;
     ctx.lineCap = 'round';
     ctx.lineJoin = 'round';
     ctx.strokeStyle = 'rgba(0, 0, 0, ' + (1 - this.shade/128) + ')';
 
+    ctx.save();
+    ctx.translate(this.p.x, this.p.y);
+    ctx.rotate(this.angle);
+    ctx.translate(-this.p.x, -this.p.y);
     ctx.beginPath();
     ctx.arc(this.p.x, this.p.y, this.r, 0, Math.PI * 2, true);
     ctx.moveTo(this.p.x - this.r * 0.30, this.p.y - this.r * 0.55);
@@ -52,7 +58,8 @@ logo.prototype.draw = function(ctx) {
     ctx.lineTo(this.p.x + this.r * 0.30, this.p.y + this.r * 0.55);
     ctx.stroke();
     ctx.closePath();
-}
+    ctx.restore();
+};
 
 function spawn(x, y) {
     var a = Math.PI / 4 + (Math.PI / 2 * Math.random()), e;
@@ -60,10 +67,10 @@ function spawn(x, y) {
         return;
     }
 
-    e = new logo(
-        new vector(x, y),
+    e = new Logo(
+        new Vector(x, y),
         10 + Math.random() * 50,
-        new vector(Math.cos(a) * 15, -Math.sin(a) * 15)
+        new Vector(Math.cos(a) * 15, -Math.sin(a) * 15)
     );
     world.push(e);
 }
@@ -78,25 +85,25 @@ function init() {
     canvas.width = CANVAS.x;
     canvas.height = CANVAS.y;
 
-    canvas.onmousedown = function(e) {
-        if (interval) {
-            return;
-        }
-        interval = setInterval(function() {
-            spawn(mx, my);
-        }, 40);
-    }
     canvas.onclick = function(e) {
         spawn(e.offsetX, e.offsetY);
-    }
-    canvas.onmousemove = function(e) {
-        mx = e.offsetX;
-        my = e.offsetY;
-    }
+    };
+    //canvas.onmousedown = function(e) {
+    //    if (interval) {
+    //        return;
+    //    }
+    //    interval = setInterval(function() {
+    //        spawn(mx, my);
+    //    }, 40);
+    //};
     canvas.onmouseup = function() {
         clearInterval(interval);
         interval = null;
-    }
+    };
+    canvas.onmousemove = function(e) {
+        mx = e.offsetX;
+        my = e.offsetY;
+    };
 
     return canvas;
 }
@@ -113,20 +120,27 @@ function run() {
                     o.shade += 1;
                     if (o.shade >= 127) {
                         o.shade = 0;
+
                         remove.unshift(i);
                     }
                 } else {
                     resultant.add(o.forces[1]);
                     o.forces[1].y += o.mass;
+                    o.angle += o.spin;
                     o.p.add(resultant);
+
                     if (o.p.y + o.r >= CANVAS.y && resultant.y > 0) {
                         resultant.y *= -B;
+                        // effect
+                        o.spin += resultant.x / 100;
                     }
                     if (
                         (o.p.x + o.r >= CANVAS.x && resultant.x > 0)
                         || (o.p.x - o.r <= 0 && resultant.x < 0
                     )) {
                         resultant.x *= -B;
+                        // effect
+                        o.spin += resultant.y / 100;
                     }
                     if (o.p.y + o.r >= CANVAS.y && Math.abs(resultant.y) <= 1) {
                         // lying on the floor now.
@@ -136,6 +150,7 @@ function run() {
                     // floor friction
                     if (o.forces[1].y == 0) {
                         resultant.x *= 0.95;
+                        o.spin = resultant.x / 15;
                         if(Math.abs(resultant.x) < 0.1) {
                             resultant.x = 0;
                         }
